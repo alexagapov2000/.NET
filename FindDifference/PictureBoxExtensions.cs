2cb7d6b32ed0a9acb5f9471e2dbd26503a15787e
Alexey Agapov <47774142+alexagapov2000@users.noreply.github.com>
2019-04-12 00:20:15 +0300

    Last changes 25.11.2018 + minor changes 11.04.2019

---
+++ FindDifference/PictureBoxExtensions.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FindDifference
+{
+    public static class PictureBoxExtensions
+    {
+        public static Point GetPointOfCursor(this PictureBox pictureBox)
+        {
+            var ratioX = (double)pictureBox.Image.Width / pictureBox.Width;
+            var ratioY = (double)pictureBox.Image.Height / pictureBox.Height;
+            var result = new Point(
+                (int)(pictureBox.PointToClient(Cursor.Position).X * ratioX),
+                (int)(pictureBox.PointToClient(Cursor.Position).Y * ratioY));
+            return result;
+        }
+    }
+}
